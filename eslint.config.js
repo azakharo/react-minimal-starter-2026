d1fcd3f5676e5b4c728c1a299436d47e1664c0d4
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-11-30 11:53:37 +0300

    added tailwind css

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,10 @@ export default defineConfig([
       eslintPluginPrettierRecommended,
     ],
     languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
