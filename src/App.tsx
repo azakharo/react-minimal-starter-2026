5cfcd7a854401fe0598f98107a53c4841134cfa0
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-06-13 17:27:49 +0300

    added agentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
 import {useState} from 'react';
 import {Button} from '@/components/ui/button';
+import {Agentation} from 'agentation';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="mx-auto flex max-w-md items-center justify-center">
-      <div>
-        <h1 className="mb-4 text-center text-4xl">Vite + React</h1>
-        <div className="flex justify-center">
-          <Button
-            className="
-              bg-green-600 text-white
-              hover:bg-green-700
-            "
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is {count}
-          </Button>
-        </div>
-        <div className="mt-2 text-2xl text-green-600">
-          This template uses Tailwind CSS
+    <>
+      <div className="mx-auto flex max-w-md items-center justify-center">
+        <div>
+          <h1 className="mb-4 text-center text-4xl">Vite + React</h1>
+          <div className="flex justify-center">
+            <Button
+              className="
+                bg-green-600 text-white
+                hover:bg-green-700
+              "
+              onClick={() => setCount(count => count + 1)}
+            >
+              count is {count}
+            </Button>
+          </div>
+          <div className="mt-2 text-2xl text-green-600">
+            This template uses Tailwind CSS
+          </div>
         </div>
       </div>
-    </div>
+
+      <Agentation />
+    </>
   );
 }
 
